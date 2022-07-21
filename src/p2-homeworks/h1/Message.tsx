f12574246a44aa2826b85852f7168de0d6140579
timgaydarov <gaidarov_timur@mail.ru>
2022-07-21 21:13:21 +0300

    last commit 2

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import classes from "./Message.module.css";
 
-function Message() {
-    return (
-        <div>
+type MessageDataType = {
+  avatar: any
+  name: string
+  message: string
+  time: any
+}
 
+function Message(props: MessageDataType) {
+  return (
+    <div className={classes.message}>
+      <div>
+        <img className={classes.avatar} src={props.avatar}/>
+      </div>
+      <div className={classes.dialog_window}>
+        <div className={classes.name}>
+          <span>{props.name}</span>
         </div>
-    )
+        <div>
+          <span className={classes.user_message}>{props.message}</span><span className={classes.time}>{props.time}</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Message
